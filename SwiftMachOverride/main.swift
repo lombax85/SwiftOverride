382cd5bd61277ce5f0eb957c6d3c7b9be70ac50c
Fabio Lombardo <lombax85@gmail.com>
2019-05-26 20:40:49 +0200

    Initial Commit

---
+++ SwiftMachOverride/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftMachOverride
+//
+//  Created by Fabio Lombardo on 26/05/2019.
+//  Copyright © 2019 Fabio Lombardo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
